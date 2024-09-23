9b7f37fa06e02a48aa9eacd9c4ff25022e23290a
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-09-24 03:21:20 +0530

    fix

--- plugins/ping.js
+++ plugins/ping.js
@@ -27,18 +27,19 @@ eypz({
     category: "main",
     filename: __filename
 },
-async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-    const AmeenT = '📍ping...'
-      m.react(config.wait)
-          const AmeenS = await conn.sendMessage(from, { text: AmeenT }, { quoted: mek }); 
-    const end = new Date().getTime();
-    const TripTime = end - start;
-    const AmeenET = '*🙈Pong!* ' + TripTime ' *ms*'
-    await conn.sendMessage(from, { text: AmeenET , edit: AmeenS.key });
-    m.react(config.done)
-}catch(e){
-console.log(e)
-reply(`${e}`)
-}
-})
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const AmeenT = '📍ping...';
+        m.react(config.wait);
+        const start = new Date().getTime(); 
+        const AmeenS = await conn.sendMessage(from, { text: AmeenT }, { quoted: mek });
+        const end = new Date().getTime();
+        const TripTime = end - start;
+        const AmeenET = '*🙈Pong!* ' + TripTime + ' *ms*';
+        await conn.sendMessage(from, { text: AmeenET, edit: AmeenS.key });
+        m.react(config.done);
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
