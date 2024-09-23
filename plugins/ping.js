e73c521f257cc6cac13a49768108d5288577b4d4
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-09-24 02:32:36 +0530

    fix

--- plugins/ping.js
+++ plugins/ping.js
@@ -3,16 +3,17 @@ const { commands, eypz } = require('../command');
 eypz({
     command: 'ping',
     category: 'mics',
-    handler: async (conn, args, message) => {
-        const { from } = message;
+    category: "main",
+    filename: __filename
+        },
+     async (conn, args, message) => {
+       try{
+           const { from } = message;
         const start = new Date().getTime();
         const edited = await conn.sendMessage(from, { text: '🏓 Pinging...' }, { quoted: message });
 
         const end = new Date().getTime();
         const TripTime = end - start;
-        await conn.sendMessage(from, {
-            text: `🏓 Pong!: \`${TripTime} ms`,
-            edit: edited.key 
-        });
+        await conn.sendMessage(from, { text: `🏓 Pong!: \`${TripTime} ms`, edit: edited.key });
     }
 });
