65f7cb89d094018aed33bed0aa3babcbbcf5de2b
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-09-24 03:05:50 +0530

    Ping fix ✨ [Ameen]

--- plugins/ping.js
+++ plugins/ping.js
@@ -22,20 +22,21 @@ reply(`${e}`)
 }); */
 
 eypz({
-    pattern: "alive",
-    desc: "Check bot online or no.",
+    pattern: "ping",
+    desc: "Check Bot's Performance.",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     const AmeenT = '📍ping...'
-      m.react(config.load)
+      m.react(config.wait)
           const AmeenS = await conn.sendMessage(from, { text: AmeenT }, { quoted: mek }); 
     const end = new Date().getTime();
     const TripTime = end - start;
     const AmeenET = '*🙈Pong!* ' + TripTime ' *ms*'
     await conn.sendMessage(from, { text: AmeenET , edit: AmeenS.key });
+    m.react(config.done)
 }catch(e){
 console.log(e)
 reply(`${e}`)
