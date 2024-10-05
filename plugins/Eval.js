618d6723d4768faf8b05be42acf6718c75795d78
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-06 00:24:57 +0530

    Update Eval.js

--- plugins/Eval.js
+++ plugins/Eval.js
@@ -1,45 +1,47 @@
-const config = require('../config')
-const {
-	eypz,
-	commands
-} = require('../command')
-const util = require('util')
+const syntaxerror = require('syntax-error');
+const { eypz, commands } = require('../command');
 
 eypz({
-		on: "text",
-		filename: __filename
-	},
-	async (conn, mek, m, {
-		from,
-		quoted,
-		body,
-		isCmd,
-		command,
-		args,
-		q,
-		isGroup,
-		sender,
-		senderNumber,
-		botNumber2,
-		botNumber,
-		pushname,
-		isMe,
-		isOwner,
-		groupMetadata,
-		groupName,
-		participants,
-		groupAdmins,
-		isBotAdmins,
-		isAdmins,
-		reply
-	}) => {
-		if (args.startsWith(">")) {
-			try {
-				let evaled = await eval(`(async () => { ${args.replace(">", "")} })()`);
-				if (typeof evaled !== "string") evaled = util.inspect(evaled);
-				await reply(`${evaled}`)
-			} catch (err) {
-				await reply(`${util.format(err)}`);
-			}
-		}
-	})
+    pattern: "eval",
+    desc: "Evaluate JavaScript code",
+    category: "owner",
+    filename: __filename
+},
+async (conn, mek, m, { from, body, sender, isOwner, reply }) => {
+    
+
+    let _return;
+    let _syntax = '';
+    let _text = body.slice(5).trim();
+    let oldExp = m.exp * 1;
+
+    try {
+        let i = 15;
+        let exec = new (async () => {}).constructor(
+            'print', 'm', 'conn', 'require', 'process', 'args',
+            _text
+        );
+
+        _return = await exec.call(
+            conn,
+            (...args) => {
+                if (--i < 1) return;
+                console.log(...args);
+                return conn.reply(from, require('util').format(...args), mek);
+            },
+            m, conn, require, process, body
+        );
+    } catch (e) {
+        let err = syntaxerror(_text, 'Execution Function', {
+            allowReturnOutsideFunction: true,
+            allowAwaitOutsideFunction: true,
+            sourceType: 'module',
+        });
+
+        if (err) _syntax = '```' + err + '```\n\n';
+        _return = e;
+    } finally {
+        conn.reply(from, _syntax + require('util').format(_return), mek);
+        m.exp = oldExp;
+    }
+});
