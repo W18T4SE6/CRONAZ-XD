23638769c52dd960205675481b52c53d5e8e1814
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-10 18:30:45 +0530

    Create neko.js

---
+++ plugins/neko.js
@@ -0,0 +1,11 @@
+var axios = require('axios');
+eypz({
+    pattern: "neko",
+    desc: "random img",
+    category: "main",
+    filename: __filename
+},
+async (conn, m) => {
+    var res = await axios.get("https://ironman.koyeb.app/ironman/neko");
+    await conn.sendMessage(m.chat, { image: { url: res.data.ironman.url }, caption: "neko" });
+});
