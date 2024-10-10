1873670e3e509209bc38d822980e886a3c2705d8
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-10 18:54:04 +0530

    Create group.js

---
+++ plugins/group.js
@@ -0,0 +1,50 @@
+const config = require('../config')
+const {eypz , commands} = require('../command')
+
+eypz({
+    pattern: "xinvite",
+    desc: "use for group link.",
+    category: "group",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+
+////FUNCTIONS✅️✅️////////
+
+m.me = conn.user.id.includes(':') ? conn.user.id.split(':')[0]+'@s.whatsapp.net' : client.user.id;
+ 
+m.isAdmin = async (who) => {
+ let group = await conn.groupMetadata(m.chat);
+ let participant = group.participants.filter(p => p.id == who);
+ if (participant.length != 0) return (participant[0].admin === 'superadmin' || participant[0].admin === 'admin') ? true : false;   
+ else return false;
+}
+
+
+
+////FUNCTIONS✅️✅️////////
+
+
+
+
+let AXL = m.me;
+let Y = m.sender;
+
+
+if (!await m.isGroup) return m.reply("_This is a group command!_")
+
+        if (!await m.isAdmin(AXL)) return m.reply("_I'm not an admin_")
+
+        if (!await m.isAdmin(Y)) return m.reply("_This command for only group admins_")
+        let code = await conn.groupInviteCode(m.chat)
+        return m.reply('https://chat.whatsapp.com/' + code)
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
