787d75c67cedcdd81694c8c5c8ac41ff30beb521
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-11 01:10:47 +0530

    Mee Added

--- plugins/group.js
+++ plugins/group.js
@@ -135,3 +135,19 @@ console.log(e)
 reply(`${e}`)
 }
 })
+
+eypz({
+    pattern: "mee",
+    desc: "mention me message.",
+    category: "group",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    var fek = (`@${m.sender.split("@")[0]}`)
+
+ m.reply(fek)
+    }catch(e){
+console.log(e)
+reply(`${e}`)
+} })
