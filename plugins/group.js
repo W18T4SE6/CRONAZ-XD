942ba2e16e8d1dd0f2108009b78c66795d4fb47d
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-11 15:58:48 +0530

    ✅️ Axl

--- plugins/group.js
+++ plugins/group.js
@@ -51,6 +51,76 @@ reply(`${e}`)
 
 
 
+eypz({
+    pattern: "revoke",
+    desc: "use for group link.",
+    category: "group",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+
+////FUNCTIONS✅️✅️////////
+
+m.me = conn.user.id.includes(':') ? conn.user.id.split(':')[0]+'@s.whatsapp.net' : client.user.id;
+ 
+m.isAdmin = async (who) => {
+ let group = await conn.groupMetadata(m.chat);
+ let participant = group.participants.filter(p => p.id == who);
+ if (participant.length != 0) return (participant[0].admin === 'superadmin' || participant[0].admin === 'admin') ? true : false;   
+ else return false;
+}
+
+
+
+////FUNCTIONS✅️✅️////////
+
+m.me = conn.user.id.includes(':') ? conn.user.id.split(':')[0]+'@s.whatsapp.net' : client.user.id;
+ 
+m.isAdmin = async (who) => {
+ let group = await conn.groupMetadata(m.chat);
+ let participant = group.participants.filter(p => p.id == who);
+ if (participant.length != 0) return (participant[0].admin === 'superadmin' || participant[0].admin === 'admin') ? true : false;   
+ else return false;
+}
+
+
+
+////FUNCTIONS✅️✅️////////
+
+
+
+
+let AXL = m.me;
+let Y = m.sender;
+
+
+if (!await m.isGroup) return m.reply("_This is a group command!_")
+
+        if (!await m.isAdmin(AXL)) return m.reply("_I'm not an admin_")
+
+        if (!await m.isAdmin(Y)) return m.reply("_This command for only group admins_")
+
+
+const code = await conn.groupRevokeInvite(m.chat)
+
+let ajsalxx = (`_New Group Link_\n\nhttps://chat.whatsapp.com/${code}`)
+
+let axlui = m.sender;
+
+await conn.sendMessage(axlui, { text: ajsalxx }, { quoted: mek });
+
+        return m.reply("_Group link Revoked_");
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+
 
 eypz({
     pattern: "mute",
