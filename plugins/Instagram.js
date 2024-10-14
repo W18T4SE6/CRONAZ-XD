c603f1b02f1180c7fcb019358737cb407eed9918
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-15 02:51:52 +0530

    Insta Fix ✨ [Ameen]

--- plugins/Instagram.js
+++ plugins/Instagram.js
@@ -8,8 +8,9 @@ eypz({
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-try{
-  if (!m.quoted) return `Please Replay To A Valid Insta Link🌸`
+if (!m.quoted) return `Please Replay To A Valid Insta Link🌸`
+    try{
+  
 const igdl = `https://ameen-api.vercel.app/insta/v7/igdl?url=`
 let text = m.quoted.msg
 let fet = igdl + text
