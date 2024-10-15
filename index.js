b1e26c6e58a8b08b3776f1d6bd5317b844de0f40
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-15 22:35:43 +0530

    Fixed

--- index.js
+++ index.js
@@ -96,6 +96,11 @@ console.log('Connected')
   }
 })
 conn.ev.on('creds.update', saveCreds)  
+        if (config.MODE === "public") {
+            conn.public = true;
+        } else if (config.MODE === "private") {
+            conn.public = false;
+        }
 
 conn.ev.on('messages.upsert', async(mek) => {
 mek = mek.messages[0]
