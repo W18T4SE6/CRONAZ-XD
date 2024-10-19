50e78fb09f2ec1ef7d07f9a5fa28886a94879080
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-20 02:06:57 +0530

    Update Main.js

--- plugins/Main.js
+++ plugins/Main.js
@@ -1,1 +1,24 @@
+const config = require('../config')
+const {eypz , commands} = require('../command')
 
+
+// owner join 
+
+eypz({
+    pattern: "join",
+    desc: "bot join to group",
+    category: "group",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, 
+		    isCmd, command, args, 
+		    q, isGroup, sender, 
+		    senderNumber, botNumber2, 
+		    botNumber, pushname, isMe, 
+		    isOwner, groupMetadata, 
+		    groupName, participants, 
+		    groupAdmins, isBotAdmins, 
+		    isAdmins, reply}) => {
+	try{
+var CalciFer = 
+var nimmi = await conn.groupAcceptInvite(code)
