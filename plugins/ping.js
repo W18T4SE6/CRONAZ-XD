779a10b6fd45125d4b79d6360f2e1cb93496cbc9
ᴀᴍᴇᴇɴ ɴɪɴᴊᴀ <143019367+AmeenRepo@users.noreply.github.com>
2024-10-20 02:45:37 +0530

    Ping Fix [Ameen]🥳

--- plugins/ping.js
+++ plugins/ping.js
@@ -36,7 +36,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const AmeenS = await conn.sendMessage(from, { text: AmeenT }, { quoted: mek });
         const end = new Date().getTime();
         const TripTime = end - start;
-        const AmeenET = '*🍁ᴄʀᴏɴᴇx-xᴅ ꜱᴩᴇᴇᴅ!* ' + TripTime + ' *ms*';
+        const AmeenET = '*🍁ꜱᴩᴇᴇᴅ!* ' + TripTime + ' *ms*';
         await conn.sendMessage(from, { text: AmeenET, edit: AmeenS.key });
         m.react(config.done);
     } catch (e) {
