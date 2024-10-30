0495633057d669bd62a6e82a1942b1382947c19a
Ajsal sparky <130032226+A-J-S-A-L-S-P-A-R-K-Y@users.noreply.github.com>
2024-10-30 19:17:45 +0530

    ✅️ Axl

---
+++ plugins/ytdl.js
@@ -0,0 +1,74 @@
+const {eypz , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+const config = require('../config')
+
+
+eypz({
+    pattern: "yts",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return m.reply("_Enter a YouTube link for download_")
+
+
+
+let res = await fetch(`https://prabath-ytdl-scrapper.koyeb.app/api/mp3v2?url=${q}`);
+
+let axl = await res.json()
+let surl = axl.dl_link
+let title = axl.title
+
+await m.reply(`*_Downloading : ${title}_*`);
+
+return await conn.sendMessage(m.chat,{audio: {url:surl},mimetype:"audio/mpeg"},{quoted:mek})
+
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
+
+
+const {eypz , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+const config = require('../config')
+
+
+eypz({
+    pattern: "song",
+    desc: "download songs",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return m.reply("_Enter a YouTube link for download_")
+
+
+
+let res = await fetch(`https://prabath-ytdl-scrapper.koyeb.app/api/mp3v2?url=${q}`);
+
+let axl = await res.json()
+let surl = axl.dl_link
+let title = axl.title
+
+await m.reply(`*_Downloading : ${title}_*`);
+
+return await conn.sendMessage(m.chat,{audio: {url:surl},mimetype:"audio/mpeg"},{quoted:mek})
+
+
+
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
