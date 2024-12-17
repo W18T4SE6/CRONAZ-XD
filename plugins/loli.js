fa4a55b7d078ae76f8e94a74589afbe5a49cc591
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-17 20:43:49 +0530

    Create loli.js

---
+++ plugins/loli.js
@@ -0,0 +1,30 @@
+const { eypz, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const fs = require('fs');
+const axios = require('axios')
+var imgmsg = "*Give me a anime name !*"
+var descgs = "It gives details of given anime name."
+var cants = "I cant find this anime."
+
+eypz({
+    pattern: "loli",
+    alias: ["imgloli"],
+    react: '🧧',
+    desc: "Download anime loli images.",
+    category: "anime",
+    use: '.loli',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let res = await axios.get('https://api.lolicon.app/setu/v2?num=1&r18=0&tag=lolicon')
+let wm = `🧧 Random loli image
+
+> *@CREATE-BY-CRONEX..*`
+await conn.sendMessage(from, { image: { url: res.data.data[0].urls.original }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
