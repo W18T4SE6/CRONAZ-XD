aae9b1beb5ffba8d9b979ecde0567def713b1d4b
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-17 20:42:20 +0530

    Create megumin.js

---
+++ plugins/megumin.js
@@ -0,0 +1,28 @@
+const { eypz, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const fs = require('fs');
+const axios = require('axios')
+var imgmsg = "*Give me a anime name !*"
+var descgs = "It gives details of given anime name."
+var cants = "I cant find this anime."
+eypz({
+    pattern: "megumin",
+    alias: ["imgmegumin"],
+    react: '🧧',
+    desc: "Download anime megumin images.",
+    category: "anime",
+    use: '.megumin',
+    filename: __filename
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let res = await axios.get('https://api.waifu.pics/sfw/megumin')
+let wm = `🧧 Random megumin image
+
+> *©CREATE-BY-CRONAZ...*`
+await conn.sendMessage(from, { image: { url: res.data.url }, caption: wm}, { quoted: mek })
+} catch (e) {
+reply(cants)
+console.log(e)
+}
+})
