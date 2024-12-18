ce40d647ac7792388e9eafad0b6cf580437168df
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-18 17:47:23 +0530

    Create broadcast.js

---
+++ plugins/broadcast.js
@@ -0,0 +1,21 @@
+const {eypz ,commands } = require('../command');
+const { exec } = require('child_process');
+const config = require('../config');
+const {sleep} = require('../lib/functions')
+eypz({
+    pattern: "broadcast",
+    desc: "Broadcast a message to all groups.",
+    category: "owner",
+    react: "📢",
+    filename: __filename
+},
+async (conn, mek, m, { from, isOwner, args, reply }) => {
+    if (!isOwner) return reply("❌ You are not the owner!");
+    if (args.length === 0) return reply("📢 Please provide a message to broadcast.");
+    const message = args.join(' ');
+    const groups = Object.keys(await conn.groupFetchAllParticipating());
+    for (const groupId of groups) {
+        await conn.sendMessage(groupId, { text: message }, { quoted: mek });
+    }
+    reply("📢 Message broadcasted to all groups.");
+});
