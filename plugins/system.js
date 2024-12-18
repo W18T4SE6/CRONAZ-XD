87883b645552b8dadf4e87c1dcd224883719fb57
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-18 17:44:54 +0530

    Create system.js

---
+++ plugins/system.js
@@ -0,0 +1,44 @@
+const config = require('../config')
+const {eypz , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+const axios = require('axios')
+const {sleep} = require('../lib/functions')
+eypz({
+    pattern: "system",
+    react: "♠️",
+    alias: ["uptime","status","runtime"],
+    desc: "cheack uptime",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let status = `
+*[ •  CRONAZ-XD - UPTIME ‎ • ]*
+*╭┈───────────────•*
+*│  ◦* *_UPTIME:➠_*
+*│  ◦* ${runtime(process.uptime())}
+*│  ◦*
+*│  ◦* *_RAM USAGE:➠_*
+*│  ◦* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*│  ◦*
+*│  ◦* *_HOSTNAME:➠_*
+*│  ◦* ${os.hostname()}
+*│  ◦*
+*│  ◦* *_PLATFORM:➠_*
+*│  ◦* ${process.env.DYNO ? "Heroku" : "Localhost"}
+*│  ◦*
+*╰┈───────────────•*
+
+*•────────────•⟢*
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴏɴᴀᴢ xᴅ
+*•────────────•⟢*
+`
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
