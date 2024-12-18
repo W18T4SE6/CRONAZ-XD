a0267d993f2acb9c2fbe051ff15ab967aa5aa303
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-18 17:48:42 +0530

    Create gjid.js

---
+++ plugins/gjid.js
@@ -0,0 +1,17 @@
+const { eypz ,commands } = require('../command');
+const { exec } = require('child_process');
+const config = require('../config');
+const {sleep} = require('../lib/functions')
+eypz({
+    pattern: "gjid",
+    desc: "Get the list of JIDs for all groups the bot is part of.",
+    category: "owner",
+    react: "📝",
+    filename: __filename
+},
+async (conn, mek, m, { from, isOwner, reply }) => {
+    if (!isOwner) return reply("❌ You are not the owner!");
+    const groups = await conn.groupFetchAllParticipating();
+    const groupJids = Object.keys(groups).join('\n');
+    reply(`📝 *Group JIDs:*\n\n${groupJids}`);
+});
