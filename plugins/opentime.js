3a024e4c2009f882b7c7d1b5f73da72e186d51b8
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-20 18:34:57 +0530

    Create opentime.js

---
+++ plugins/opentime.js
@@ -0,0 +1,74 @@
+const config = require('../config')
+const { eypz, commands } = require('../command')
+eypz({
+    pattern: "opentime",
+    react: "🔖",
+    desc: "To open group to a time",
+    category: "group",
+    use: '.opentime',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{   
+if (!isGroup) return reply(ONLGROUP)
+if (!isAdmins) return reply(ADMIN)	
+  if (args[1] == 'second') {
+                    var timer = args[0] * `1000`
+                } else if (args[1] == 'minute') {
+                    var timer = args[0] * `60000`
+                } else if (args[1] == 'hour') {
+                    var timer = args[0] * `3600000`
+                } else if (args[1] == 'day') {
+                    var timer = args[0] * `86400000`
+                } else {
+                    return reply('*select:*\nsecond\nminute\nhour\n\n*example*\n10 second')
+                }
+                reply(`Open time ${q} starting from now`)
+                setTimeout(() => {
+                    var nomor = mek.participant
+                    const open = `*OPEN TIME* THE GROUP WAS OPENED BY CRONAZ-XD V1 TO APPROVED ADMIN\n NOW MEMBERS CAN SEND MESSAGES 🔓`
+                    conn.groupSettingUpdate(from, 'not_announcement')
+                    reply(open)
+                }, timer)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
+eypz({
+    pattern: "closetime",
+    react: "🔖",
+    desc: "To close group to a time",
+    category: "group",
+    use: '.closstime',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{   
+if (!isGroup) return reply(ONLGROUP)
+if (!isAdmins) return reply(ADMIN)	
+                if (args[1] == 'second') {
+                    var timer = args[0] * `1000`
+                } else if (args[1] == 'minute') {
+                    var timer = args[0] * `60000`
+                } else if (args[1] == 'hour') {
+                    var timer = args[0] * `3600000`
+                } else if (args[1] == 'day') {
+                    var timer = args[0] * `86400000`
+                } else {
+                    return reply('*select:*\nsecond\nminute\nhour\n\n*Example*\n10 second')
+                }
+                reply(`Close time ${q} starting from now`)
+                setTimeout(() => {
+                    var nomor = m.participant
+                    const close = `*CLOSE TIME* GROUP CLOSED BY CRONAZ-XD V1 AT APPROVED ADMIN\nNOW ONLY ADMIN CAN SEND MESSAGES 🔐`
+                    conn.groupSettingUpdate(from, 'announcement')
+                    reply(close)
+                }, timer)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Error !!*')
+l(e)
+}
+})
