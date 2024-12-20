29c15e76b0e2e89e70d494382d77be8416e19c23
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2024-12-20 18:30:20 +0530

    Create tts.js

---
+++ plugins/tts.js
@@ -0,0 +1,23 @@
+const {eypz , commands} = require('../command')
+const { Sticker, createSticker, StickerTypes } = require('../command')
+const googleTTS = require('google-tts-api')
+eypz({
+    pattern: "tts",
+    desc: "download songs",
+    category: "convert",
+    react: "👧",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Need some text.")
+    const url = googleTTS.getAudioUrl(q, {
+  lang: 'hi-IN',
+  slow: false,
+  host: 'https://translate.google.com',
+})
+await conn.sendMessage(from, { audio: { url: url }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek })
+    }catch(a){
+reply(`${a}`)
+}
+})
