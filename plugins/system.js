abbd0fedbdbb2ef6f4ffadb6fe17f2a9359f93f6
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2025-03-04 17:00:28 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -15,26 +15,9 @@ eypz({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `
-*[ •  CRONAZ-XD - UPTIME ‎ • ]*
-*╭┈───────────────•*
-*│  ◦* *_UPTIME:➠_*
-*│  ◦* ${runtime(process.uptime())}
-*│  ◦*
-*│  ◦* *_RAM USAGE:➠_*
-*│  ◦* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*│  ◦*
-*│  ◦* *_HOSTNAME:➠_*
-*│  ◦* ${os.hostname()}
-*│  ◦*
-*│  ◦* *_PLATFORM:➠_*
-*│  ◦* ${process.env.DYNO ? "Heroku" : "Localhost"}
-*│  ◦*
-*╰┈───────────────•*
+  _${runtime(process.uptime())}_
 
-*•────────────•⟢*
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴄʀᴏɴᴀᴢ xᴅ
-*•────────────•⟢*
-`
+
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
 
 }catch(e){
