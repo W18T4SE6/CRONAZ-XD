04f9837e10591a05333ac756933b46ee6df9ce06
𝐂ʀᴏɴᴀᴢ <klwhiteser@gmail.com>
2025-03-05 00:15:14 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -15,7 +15,7 @@ eypz({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 let status = `
-  _${runtime(process.uptime())}_
+  _${runtime(process.runtime())}_
 
 
 await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:`${status}`},{quoted:mek})
